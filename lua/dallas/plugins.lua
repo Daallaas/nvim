b0a9c8aa5450df898d2963c149826c1e9e912ab0
Daallaas <dallas@cyberlink.top>
2023-02-26 15:28:39 +0800

    add telescope

--- lua/dallas/plugins.lua
+++ lua/dallas/plugins.lua
@@ -14,16 +14,28 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   -- put plugins here
+  -- plugins manager
   use 'wbthomason/packer.nvim'
+  -- tokyonight theme
   use 'folke/tokyonight.nvim'
+  -- git
   use 'tpope/vim-fugitive'
+  -- undo tree
   use 'mbbill/undotree'
+  -- fuzzy finder
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+  -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  -- signs for git 
   use {
     'lewis6991/gitsigns.nvim',
     config = function()
       require('gitsigns').setup()
     end
   } 
+  -- syntax highlight
   use {
       'nvim-treesitter/nvim-treesitter',
       run = function()
