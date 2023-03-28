60ca2c5327872367d803f4220b4757148d8bf005
Dallas <dallas@cyberlink.top>
2023-03-29 00:15:14 +0800

    add prettier

--- lua/dallas/plugins.lua
+++ lua/dallas/plugins.lua
@@ -43,6 +43,8 @@ return require('packer').startup(function(use)
           ts_update()
       end,
   }
+  -- prettify
+  use {'prettier/vim-prettier'}
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -114,6 +114,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/huwenjun/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-prettier"] = {
+    loaded = true,
+    path = "/Users/huwenjun/.local/share/nvim/site/pack/packer/start/vim-prettier",
+    url = "https://github.com/prettier/vim-prettier"
   }
 }
 
